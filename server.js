b16358d59632c14955a72215d1fe44df5692c46a
Adam A Prasetya <adamworks23@gmail.com>
2022-09-09 06:03:45 +0700

    [core][routes] - add routes v1 in server.js

--- server.js
+++ server.js
@@ -8,7 +8,40 @@ const app = express()
 
 //make routes
 app.get('/', (req, res) => {
-  res.send('hello from express')
+  //response html/string
+  //res.send('hello from express')
+
+  //response json
+  //res.send({ name: 'adam' })
+
+  //chaining response
+  res.status(200).json({ status: 'success', data: { id: 1 } })
+})
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ status: 'success', msg: 'show all' })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `display bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ status: 'success', msg: 'created' })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `update bootcamp ${req.params.id}` })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `delete bootcamp ${req.params.id}` })
 })
 
 const PORT = process.env.PORT || 5000
