b851582c8a617149d6d6ec0ec16517e6da535a8e
Adam A Prasetya <adamworks23@gmail.com>
2022-09-17 10:34:29 +0700

    [core] reveret server.js

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 const express = require('express')
-const dotenv = require('dotenv') //for env config
-const morgan = require('morgan') //for log
-const colors = require('colors') //cl colors
-const bodyParser = require('body-parser')
-const errorHandler = require('./middleware/error')
-const connectDB = require('./config/db')
+const dotenv = require('dotenv')
 
 //load env variables
 dotenv.config({ path: './config/config.env' })
 
-//log middleware >> replace use morgan
-//const logger = require('./middleware/logger')
-
-connectDB()
-
-//routes files
-const bootcamps = require('./routes/bootcamps')
-
-//make variable express
 const app = express()
 
-//body parser
-app.use(bodyParser.json())
+//make routes
+app.get('/', (req, res) => {
+  //response html/string
+  //res.send('hello from express')
+
+  //response json
+  //res.send({ name: 'adam' })
+
+  //chaining response
+  res.status(200).json({ status: 'success', data: { id: 1 } })
+})
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ status: 'success', msg: 'show all' })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `display bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ status: 'success', msg: 'created' })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `update bootcamp ${req.params.id}` })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', msg: `delete bootcamp ${req.params.id}` })
+})
 
 //dev logging middleware (using morgan)
 if (process.env.NODE_ENV === 'development') {
